f7d894e2a2701eea3a93d2c831730cc605551bfd
t3staa <testa.leo@outlook.com>
2025-01-13 22:49:19 -0300

    feat: creating interfaces repository

---
+++ CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,7 @@
+public interface ICategoryRepository {
+    Task<IEnumerable<Category>> GetCategoriesAsync();
+    Task<Category> GetByIdAsync(int? id);
+    Task<Category> CreateAsync(Category category); 
+    Task<Category> UpdateAsync(Category category); 
+    Task<Category> RemoveAsync(Category category); 
+}
