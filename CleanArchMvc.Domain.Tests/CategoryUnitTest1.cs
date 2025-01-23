f3a842b15add27fe7ba4df9f546ce3a2410d8eb0
t3staa <testa.leo@outlook.com>
2025-01-23 16:46:22 -0300

    feat: adding more tests for Category

--- CleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
+++ CleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
@@ -13,4 +13,35 @@ public class CategoryUnitTest1
         action.Should()
               .NotThrow<DomainExceptionValidation>();
     }
+
+    [Fact]
+    public void CreateCategory_NegativeIdValue_DomainExceptionInvalidId()
+    {
+        Action action = () => new Category(-1, "Category Name");
+        action.Should().Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+              .WithMessage("Invalid Id Value");
+    }
+    
+    [Fact]
+    public void CreateCategory_ShortNameValue_DomainExceptionShortName()
+    {
+        Action action = () => new Category(1, "Ca");
+        action.Should().Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                        .WithMessage("Invalid name. Too short, minimum 3 characters");
+    }
+
+    [Fact]
+    public void CreateCategory_MissingNameValue_DomainExceptionRequiredName()
+    {
+        Action action = () => new Category(1, "");
+        action.Should().Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                        .WithMessage("Invalid name. Name is required");
+    }
+
+    [Fact]
+    public void CreateCategory_WithNullNameValue_DomainExceptionInvalidName()
+    {
+        Action action = () => new Category(1, null);
+        action.Should().Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+    }
 }
